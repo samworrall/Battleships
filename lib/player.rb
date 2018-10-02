e55f9bf150cad0f6e744deb9c313cec294245a37
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-10-02 15:40:13 +0100

    Add fleet attr_accessor to Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,7 @@
 class Player
+  attr_accessor :fleet
+
+  def initialize
+    @fleet = []
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,10 @@
 require 'player'
 
 describe Player do
-  
+  describe '#fleet', :fleet do
+    it 'Returns an empty array upon initialisation' do
+      expect(subject.fleet).to be_empty
+      expect(subject.fleet).to eq([])
+    end
+  end
 end
