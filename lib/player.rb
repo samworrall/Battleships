bbe1c0f3f4dba75e7a813d11d7326cf7a0fb48f9
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-10-03 13:45:53 +0100

    Add construct_fleet method to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,13 @@
 class Player
   attr_accessor :fleet
 
-  def initialize
+  def initialize(ship_class)
     @fleet = []
+    @ship_class = ship_class
+  end
+
+  def construct_fleet
+    @fleet +=  [@ship_class.carrier, @ship_class.battleship, @ship_class.cruiser,
+    @ship_class.submarine, @ship_class.destroyer]
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,26 @@
 require 'player'
 
 describe Player do
+  let(:subject) { Player.new(ship_class) }
+  let(:ship_class) { double :ship_class, carrier: carrier, battleship: battleship,
+                     cruiser: cruiser, submarine: submarine, destroyer: destroyer }
+  let(:carrier) { double :carrier, size: 5 }
+  let(:battleship) { double :battleship, size: 4 }
+  let(:cruiser) { double :cruiser, size: 3 }
+  let(:submarine) { double :submarine, size: 3 }
+  let(:destroyer) { double :destroyer, size: 2 }
+
+
   describe '#fleet', :fleet do
     it 'Returns an empty array upon initialisation' do
       expect(subject.fleet).to be_empty
       expect(subject.fleet).to eq([])
+    end
+  end
+
+  describe '#construct_fleet', :construct_fleet do
+    it 'Increases the length of fleet array by 5' do
+      expect { subject.construct_fleet }.to change {subject.fleet.length }.by(5)
     end
   end
 end
