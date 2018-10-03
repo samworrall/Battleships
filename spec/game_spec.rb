5dfd760b5f4ab3e64b4477ef53c5d3ced2e91f58
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-10-03 14:00:05 +0100

    Game initialises with a player2

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,19 @@
 require 'game'
 
 describe Game do
-  let(:subject) { Game.new(player1) }
+  let(:subject) { Game.new(player1, player2) }
   let(:player1) { double :player1 }
+  let(:player2) { double :player2 }
 
   describe '#player1', :player1 do
     it 'returns player1' do
       expect(subject.player1).to eq(player1)
+    end
+  end
+
+  describe '#player2', :player2 do
+    it 'returns player2' do
+      expect(subject.player2).to eq(player2)
     end
   end
 end
